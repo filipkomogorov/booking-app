69818f4ade021e4119d0550df695604c04d0d0d3
Filip Komogorov <filipkomogorov@gmail.com>
2023-02-28 22:09:09 +0100

    adding logo, and top-nav

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Header from './components/header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-      app
-    </div>
+    <Header/>
   )
 }
 

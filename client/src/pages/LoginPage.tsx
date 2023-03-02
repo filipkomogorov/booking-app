5e361b01735533e06dfdc34e4a3a9494b6d9e64f
Filip Komogorov <filipkomogorov@gmail.com>
2023-03-02 22:04:13 +0100

    adding yup and formik

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import { loginSchema } from "../schemas/LoginValidation";
+import { useFormik } from "formik";
+
+const onSubmit = () => {
+  console.log("submitted");
+};
 
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
+  const { values, errors, touched, handleChange, handleBlur, handleSubmit } =
+    useFormik({
+      initialValues: {
+        email: "",
+        password: "",
+      },
+      validationSchema: loginSchema,
+      onSubmit,
+    });
 
-export default LoginPage
+  console.log(errors);
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        {/* TODO Create custom text component */}
+        <label>Email:</label>
+        <input
+          name="email"
+          type="email"
+          onChange={handleChange}
+          value={values.email}
+          onBlur={handleBlur}
+          className={`border border-2 ${
+            errors.email && touched.email ? "border-error" : ""}`}
+        />
+        {errors.email && touched.email && <span className="text-error">{errors.email}</span>}
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          onChange={handleChange}
+          value={values.password}
+          onBlur={handleBlur}
+          className={`border border-2 ${
+            errors.password && touched.password ? "border-error" : ""
+          }`}
+        />
+        {errors.password && touched.password && <span className="text-error">{errors.password}</span>}
+
+        <button type="submit" className="border">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,14 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
     colors: {
-      cta: '#E4002B',
-      white: '#ffffff'
-    }
+      cta: "#E4002B",
+      white: "#ffffff",
+      error: "#fc8181",
+    },
   },
   plugins: [],
-}
+};

9731795ca44eb124f26d32671c1b16d31d2ae5cb
Filip Komogorov <filipkomogorov@gmail.com>
2023-03-04 09:15:54 +0100

    setting up basic formik login and hooking to yup

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { loginSchema } from "../schemas/LoginValidation";
-import { useFormik } from "formik";
+import { useFormik, FormikHelpers } from "formik";
+import { Login as loginSchemaTypes } from "../schemas/LoginValidation";
 
-const onSubmit = () => {
-  console.log("submitted");
+const onSubmit = async (
+  values: loginSchemaTypes,
+  actions: FormikHelpers<loginSchemaTypes>
+) => {
+  console.log(values);
+  console.log(actions);
+  await new Promise((resolve)=>setTimeout(resolve, 1000))
+  actions.resetForm()
 };
 
 const LoginPage = () => {
-  const { values, errors, touched, handleChange, handleBlur, handleSubmit } =
+  const { values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting } =
     useFormik({
       initialValues: {
         email: "",
@@ -17,10 +24,9 @@ const LoginPage = () => {
       onSubmit,
     });
 
-  console.log(errors);
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className='flex flex-col max-w-2xl mx-auto gap-3'>
         {/* TODO Create custom text component */}
         <label>Email:</label>
         <input
@@ -30,9 +36,12 @@ const LoginPage = () => {
           value={values.email}
           onBlur={handleBlur}
           className={`border border-2 ${
-            errors.email && touched.email ? "border-error" : ""}`}
+            errors.email && touched.email ? "border-error" : ""
+          }`}
         />
-        {errors.email && touched.email && <span className="text-error">{errors.email}</span>}
+        {errors.email && touched.email && (
+          <span className="text-error">{errors.email}</span>
+        )}
         <label>Password:</label>
         <input
           type="password"
@@ -44,10 +53,12 @@ const LoginPage = () => {
             errors.password && touched.password ? "border-error" : ""
           }`}
         />
-        {errors.password && touched.password && <span className="text-error">{errors.password}</span>}
-
-        <button type="submit" className="border">
-          Submit
+        {errors.password && touched.password && (
+          <span className="text-error">{errors.password}</span>
+        )}
+        {/* TODO replace the text inside with spinner when isSubmitting */}
+        <button disabled={isSubmitting} type="submit" className={`py-3 rounded-lg ${isSubmitting ? 'border bg-grey-300 text-black' : 'bg-cta text-white'}`}>
+          Log in
         </button>
       </form>
     </div>

baf7cdb26dbfba4291d1f4efe0e2d7a83c1b40be
Filip Komogorov <filipkomogorov@gmail.com>
2023-04-01 12:06:31 +0200

    Context for adding property

---
+++ client/src/context/PropertyContext.tsx
@@ -0,0 +1,32 @@
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+
+import { PropertyData } from '../models/Property';
+
+type PropertyContextType = {
+  propertyData: PropertyData | undefined;
+  setPropertyData: (propertyData: PropertyData) => void;
+};
+
+const PropertyContext = createContext<PropertyContextType | undefined>(undefined);
+
+type PropertyProviderProps = {
+  children: ReactNode;
+};
+
+export const PropertyDataProvider: React.FC<PropertyProviderProps> = ({ children }) => {
+  const [propertyData, setPropertyData] = useState<PropertyData | undefined>(undefined);
+
+  return (
+    <PropertyContext.Provider value={{ propertyData, setPropertyData }}>
+      {children}
+    </PropertyContext.Provider>
+  );
+};
+
+export const usePropertyData = () => {
+  const context = useContext(PropertyContext);
+  if (context === undefined) {
+    throw new Error('usePropertyData must be used within a PropertyDataProvider');
+  }
+  return context;
+};
--- client/src/pages/AddProeprty.tsx
+++ client/src/pages/AddProeprty.tsx
@@ -1,12 +1,15 @@
-import Wizard from "../components/Wizard/Wizard"
+import Wizard from "../components/Wizard/Wizard";
+import { PropertyDataProvider } from "../context/PropertyContext";
 
 const AddProeprty = () => {
   return (
-    <div className='w-desktop mx-auto mt-sizeXl bg-white p-sizeMedium'>
-        <h2 >Let's add a Property</h2>
+    <div className="w-desktop mx-auto mt-sizeXl bg-white p-sizeMedium">
+      <h2>Let's add a Property</h2>
+      <PropertyDataProvider>
         <Wizard />
+      </PropertyDataProvider>
     </div>
-  )
-}
+  );
+};
 
-export default AddProeprty
+export default AddProeprty;

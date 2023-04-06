37ad4fe1376867abaca769b01080a479e736e073
Filip Komogorov <filipkomogorov@gmail.com>
2023-04-06 14:57:37 +0200

    Adding loading screen to Signup and Login page

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -8,24 +8,27 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
+import Loading from "../components/Loading/Loading";
 
 const LoginPage: React.FC = () => {
   const navigate = useNavigate();
   const { user, setUser } = useContext(UserContext);
   // TODO add enum for different errors
-  const [loginErr, setLoginErr] = useState(false)
+  const [loginErr, setLoginErr] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
 
-  useEffect(()=>{
-    if(user){
-      navigate('/')
+  useEffect(() => {
+    if (user) {
+      navigate("/");
     }
-  }, [user])
+  }, [user]);
 
   const onSubmit = async (
     values: loginSchemaTypes,
     actions: FormikHelpers<loginSchemaTypes>
   ) => {
-    setLoginErr(false)
+    setLoginErr(false);
+    setLoading(true);
     // TODO make it a login hook
     try {
       const { email, password } = values;
@@ -37,57 +40,70 @@ const LoginPage: React.FC = () => {
         },
         { withCredentials: true }
       );
+      setLoading(false);
       setUser(response.data);
       navigate("/");
     } catch (err) {
-      // TODO handle the error if its 500 
-      setLoginErr(true)
+      // TODO handle the error if its 500
+      setLoading(false);
+      setLoginErr(true);
     }
   };
 
   return (
     <div className="w-login mx-auto bg-white mt-32 p-sizeLarge rounded-3xl flex flex-col">
-      <div className="flex items-center gap-3 mx-auto mb-10">
-        <LogoSvg />
-        <p className="leading-none pt-2 text-4xl">HeavenEstate</p>
-      </div>
-      <h3 className="text-center text-3xl font-semibold pb-12">Log in</h3>
-      <Formik
-        initialValues={{
-          email: "",
-          password: "",
-        }}
-        validationSchema={loginSchema}
-        onSubmit={onSubmit}
-      >
-        {({ isSubmitting }) => (
-          <Form className="flex flex-col">
-            <TextFieldWithIcon placeholder="Email" name="email" type="email" />
-            <TextFieldWithIcon
-              placeholder="Password"
-              name="password"
-              type="password"
-            />
-            <button
-              disabled={isSubmitting}
-              type="submit"
-              className={`${
-                isSubmitting
-                  ? "border border-1 bg-grey-300 text-black"
-                  : "bg-cta text-white"
-              } rounded-xl px-7 h-20`}
-            >
-              Log in
-            </button>
-            {/* TODO - make an error message component */}
-            {loginErr && <p className="text-base">Invalid email or password</p>}
-          </Form>
-        )}
-      </Formik>
+      {loading ? (
+        <Loading isLoading={loading} />
+      ) : (
+        <>
+          <div className="flex items-center gap-3 mx-auto mb-10">
+            <LogoSvg />
+            <p className="leading-none pt-2 text-4xl">HeavenEstate</p>
+          </div>
+          <h3 className="text-center text-3xl font-semibold pb-12">Log in</h3>
+          <Formik
+            initialValues={{
+              email: "",
+              password: "",
+            }}
+            validationSchema={loginSchema}
+            onSubmit={onSubmit}
+          >
+            {({ isSubmitting }) => (
+              <Form className="flex flex-col">
+                <TextFieldWithIcon
+                  placeholder="Email"
+                  name="email"
+                  type="email"
+                />
+                <TextFieldWithIcon
+                  placeholder="Password"
+                  name="password"
+                  type="password"
+                />
+                <button
+                  disabled={isSubmitting}
+                  type="submit"
+                  className={`${
+                    isSubmitting
+                      ? "border border-1 bg-grey-300 text-black"
+                      : "bg-cta text-white"
+                  } rounded-xl px-7 h-20`}
+                >
+                  Log in
+                </button>
+                {loginErr && (
+                  <p className="text-base">Invalid email or password</p>
+                )}
+              </Form>
+            )}
+          </Formik>
 
-      <div className="flex justify-center items-center mt-10">
-        <Link to="/recover">Forgot Password?</Link>
-      </div>
+          <div className="flex justify-center items-center mt-10">
+            <Link to="/recover">Forgot Password?</Link>
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -9,23 +9,26 @@ import { Link, useNavigate } from "react-router-dom";
 import LogoSvg from "../components/Logo/LogoSvg";
 import axios from "axios";
 import { UserContext } from "../context/UserContext";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
+import Loading from "../components/Loading/Loading";
 
 const RegisterPage = () => {
   const { user, setUser } = useContext(UserContext);
+  const [loading, setLoading] = useState<boolean>(false);
   const navigate = useNavigate();
 
-  useEffect(()=>{
-    if(user){
-      navigate('/')
+  useEffect(() => {
+    if (user) {
+      navigate("/");
     }
-  }, [user])
+  }, [user]);
 
   const onSubmit = async (
     values: registerSchemaTypes,
     actions: FormikHelpers<registerSchemaTypes>
   ) => {
     // TODO make it a register hook
+    setLoading(true);
     try {
       const { email, password, firstName, lastName } = values;
       const response = await axios.post(
@@ -38,67 +41,86 @@ const RegisterPage = () => {
         },
         { withCredentials: true }
       );
+      setLoading(false);
       setUser(response.data);
       navigate("/");
     } catch (err) {
+      setLoading(false);
       // TODO handle error
       alert("registration failed");
     }
   };
 
-
   return (
     <div className="w-login mx-auto bg-white mt-32 py-10 px-20 rounded-3xl flex flex-col">
-      <div className="flex items-center gap-3 mx-auto mb-10">
-        <LogoSvg />
-        <p className="leading-none pt-2 text-4xl">HeavenEstate</p>
-      </div>
-      <h3 className="text-center text-3xl font-semibold pb-12">
-        Create an account
-      </h3>
-      <Formik
-        initialValues={{
-          email: "",
-          firstName: "",
-          lastName: "",
-          password: "",
-          confirmPassword: "",
-        }}
-        validationSchema={registrationSchema}
-        onSubmit={onSubmit}
-      >
-        {({ isSubmitting }) => (
-          <Form className="flex flex-col">
-            <TextField placeholder="Email" name="email" type="email" />
-            <TextField placeholder="First Name" name="firstName" type="text" />
-            <TextField placeholder="Last name" name="lastName" type="text" />
-            <TextField placeholder="Password" name="password" type="password" />
-            <TextField
-              placeholder="Repeat Password"
-              name="confirmPassword"
-              type="password"
-            />
-            <button
-              disabled={isSubmitting}
-              type="submit"
-              className={`${
-                isSubmitting
-                  ? "border border-1 bg-grey-300 text-black"
-                  : "bg-cta text-white"
-              } rounded-xl px-7 h-20`}
-            >
-              Register
-            </button>
-          </Form>
-        )}
-      </Formik>
+      {loading ? (
+        <Loading isLoading={loading} />
+      ) : (
+        <>
+          <div className="flex items-center gap-3 mx-auto mb-10">
+            <LogoSvg />
+            <p className="leading-none pt-2 text-4xl">HeavenEstate</p>
+          </div>
+          <h3 className="text-center text-3xl font-semibold pb-12">
+            Create an account
+          </h3>
+          <Formik
+            initialValues={{
+              email: "",
+              firstName: "",
+              lastName: "",
+              password: "",
+              confirmPassword: "",
+            }}
+            validationSchema={registrationSchema}
+            onSubmit={onSubmit}
+          >
+            {({ isSubmitting }) => (
+              <Form className="flex flex-col">
+                <TextField placeholder="Email" name="email" type="email" />
+                <TextField
+                  placeholder="First Name"
+                  name="firstName"
+                  type="text"
+                />
+                <TextField
+                  placeholder="Last name"
+                  name="lastName"
+                  type="text"
+                />
+                <TextField
+                  placeholder="Password"
+                  name="password"
+                  type="password"
+                />
+                <TextField
+                  placeholder="Repeat Password"
+                  name="confirmPassword"
+                  type="password"
+                />
+                <button
+                  disabled={isSubmitting}
+                  type="submit"
+                  className={`${
+                    isSubmitting
+                      ? "border border-1 bg-grey-300 text-black"
+                      : "bg-cta text-white"
+                  } rounded-xl px-7 h-20`}
+                >
+                  Register
+                </button>
+              </Form>
+            )}
+          </Formik>
 
-      <div className="flex justify-center items-center mt-10">
-        <p className="pr-1 ">Already have an account? </p>
-        <Link className="text-link font-bold" to="/login">
-          Log in
-        </Link>
-      </div>
+          <div className="flex justify-center items-center mt-10">
+            <p className="pr-1 ">Already have an account? </p>
+            <Link className="text-link font-bold" to="/login">
+              Log in
+            </Link>
+          </div>
+        </>
+      )}
     </div>
   );
 };

f8bece44693b9d3bfa4653f7fa6e27a378c1bc6e
Filip Komogorov <filipkomogorov@gmail.com>
2023-05-01 14:00:03 +0200

    added banner and anchors to landing page

--- client/src/pages/IndexPage.tsx
+++ client/src/pages/IndexPage.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
+import banner from "../assets/banner.jpg";
 import axios from "axios";
 import { PropertyData } from "../models/Property";
 import Card from "../components/Card/Card";
+import { Link } from "react-router-dom";
 
 type CombinedProperties = {
   sell: Array<PropertyData>;
@@ -25,52 +27,76 @@ const IndexPage = () => {
   }, []);
 
   return (
-    <div className="w-desktopWide mx-auto">
-      <h1>index page</h1>
-      <div>
+    <div className="w-desktop80 mx-auto">
+      <div
+        style={{
+          height: "57rem",
+          width: "100%",
+          backgroundImage: `url(${banner})`,
+          backgroundSize: "cover",
+          backgroundPosition: "bottom",
+          marginBottom: "3.2rem",
+          position: "relative",
+        }}
+      ></div>
+      <div className="w-desktopWide mx-auto">
         <div>
-          <h2>Latest properties for sale</h2>
-          <div
-            style={{
-              display: "flex",
-              width: "100%",
-              flexWrap: "wrap",
-              gap: "2rem",
-              justifyContent: "space-between",
-            }}
-          >
-            {data &&
-              data.sell.map((el, index) => <Card property={el} key={index} />)}
+          <div>
+            <h2>Latest properties for sale</h2>
+            <div
+              style={{
+                display: "flex",
+                width: "100%",
+                flexWrap: "wrap",
+                gap: "2rem",
+                justifyContent: "space-between",
+              }}
+            >
+              {data &&
+                data.sell.map((el, index) => (
+                  <Card property={el} key={index} />
+                ))}
+              <div className="flex items-center">
+                <Link to="/buy" style={{ textDecoration: "underline" }}>
+                  See all{" "}
+                </Link>
+              </div>
+            </div>
           </div>
-        </div>
 
-        <div
-          style={{
-            width: "80%",
-            height: "3px",
-            backgroundColor: "rgba(216,216,216,0.8)",
-            display: "block",
-            borderRadius: "5px",
-            margin: "3.2rem auto 3.2rem 0",
-          }}
-        >
-          {/* separator line */}
-        </div>
-        <div>
-          <h2>Latest properties for rent</h2>
           <div
             style={{
-              width: "100%",
-              display: "flex",
-              flexWrap: "wrap",
-              gap: "2rem",
-              justifyContent: "space-between",
+              width: "80%",
+              height: "3px",
+              backgroundColor: "rgba(216,216,216,0.8)",
+              display: "block",
+              borderRadius: "5px",
+              margin: "3.2rem auto 3.2rem 0",
             }}
           >
-            {data &&
-              data.rent.map((element, index) => (
-                <Card property={element} key={index} />
-              ))}
+            {/* separator line */}
+          </div>
+          <div>
+            <h2>Latest properties for rent</h2>
+            <div
+              style={{
+                width: "100%",
+                display: "flex",
+                flexWrap: "wrap",
+                gap: "2rem",
+                justifyContent: "space-between",
+              }}
+            >
+              {data &&
+                data.rent.map((element, index) => (
+                  <Card property={element} key={index} />
+                ))}
+              <div className="flex items-center">
+                <Link to="/rent" style={{ textDecoration: "underline" }}>
+                  See all{" "}
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
